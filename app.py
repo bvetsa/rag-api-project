9ae8ce3fa49cf7aa71e0aca05753d7d03866fd33
Bhargava Vetsa <bhargava.vetsa@gmail.com>
2026-02-01 01:39:20 -0500

    fixed docker issues

--- app.py
+++ app.py
@@ -17,16 +17,18 @@ logging.info(f"Using model: {MODEL_NAME}")
 app = FastAPI()
 chroma = chromadb.PersistentClient(path="./db")
 collection = chroma.get_or_create_collection("docs")
+ollama_client = ollama.Client(host="http://host.docker.internal:11434")
 
 @app.post("/query")
 def query(q: str):
     results = collection.query(query_texts=[q], n_results=1)
     context = results["documents"][0][0] if results["documents"] else ""
 
-    answer = ollama.generate(
-        model=MODEL_NAME,
-        prompt=f"Context:\n{context}\n\nQuestion: {q}\n\nAnswer clearly and concisely:"
-    )
+    answer = ollama_client.generate(
+    model="tinyllama",
+    prompt=f"Context:\n{context}\n\nQuestion: {q}\n\nAnswer clearly and concisely:"
+)
+
 
     logging.info(f"/query asked: {q}")
 
